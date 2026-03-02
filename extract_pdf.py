f666b8dda18bb2aacc8841cff194316ddf6a80ce
Ramzi <hadad@MacBook-Pro-van-Ramzi.local>
2026-03-02 14:47:48 +0100

    chore: remove non-essential files from production repo

--- extract_pdf.py
+++
@@ -1,8 +0,0 @@
-from pdfminer.high_level import extract_text
-import sys
-
-try:
-    text = extract_text(sys.argv[1])
-    print(text)
-except Exception as e:
-    print(f"Error: {e}", file=sys.stderr)
