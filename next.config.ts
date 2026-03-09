4cfabdd281fd47baffd54e0d60022c68f70a1f8f
Ramzi <hadad@MBP-van-Ramzi.home>
2026-03-09 21:18:48 +0100

    fix theroadto redirect

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,16 @@
+nano next.config.tsconst nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/theroadto",
+        destination: "/theroadto/index.html",
+        permanent: false,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
 const nextConfig = {
   async redirects() {
     return [
